df1ff7c84aa97481ef11a2ed111e61827c95663f
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-31 10:38:46 -0400

    Initial Commit

---
+++ ShitBrowser/ShitBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShitBrowserApp.swift
+//  ShitBrowser
+//
+//  Created by Hariz Shirazi on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct ShitBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShitBrowserUITests/ShitBrowserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShitBrowserUITestsLaunchTests.swift
+//  ShitBrowserUITests
+//
+//  Created by Hariz Shirazi on 2023-03-31.
+//
+
+import XCTest
+
+final class ShitBrowserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
