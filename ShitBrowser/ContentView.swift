b8d5048112939e0557f3c805a060036207430e30
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-31 11:10:04 -0400

    ok done

--- ShitBrowser/ContentView.swift
+++ ShitBrowser/ContentView.swift
@@ -6,14 +6,44 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
 
 struct ContentView: View {
+    @State var currentPage: URL = URL(string: "https://bomberfish.ca")!
+    @State var newPage: String = "https://bomberfish.ca"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            WebView(url: currentPage)
+                .cornerRadius(16)
+            HStack {
+                TextField(text: $newPage, prompt: Text("Enter website URL...")) {
+                    Text("Enter website URL...")
+                }
+                .cornerRadius(16)
+                .padding()
+                .background{
+                    Color(UIColor.secondarySystemBackground)
+                        .cornerRadius(16)
+                }
+                Button(action: {
+                    currentPage = URL(string: newPage)!
+                }, label: {
+                    Label("Go", systemImage: "arrow.right")
+                })
+            }
         }
         .padding()
     }
